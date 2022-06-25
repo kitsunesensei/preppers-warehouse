8eb854a1b1f6a1cba1e6d9c8a1f8262c44b67e06
David Kurt <redfox@live.de>
2022-06-25 15:51:59 +0200

    feat: installed open sans and monospace font

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { ViteWebfontDownload } from 'vite-plugin-webfont-dl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    ViteWebfontDownload([
+      'https://fonts.googleapis.com/css2?family=Courier+Prime&family=Open+Sans&display=swap',
+    ]),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
