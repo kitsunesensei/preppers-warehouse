4408155a4c9431de78b440608a06d654b7f783a6
David Kurt <redfox@live.de>
2022-06-27 14:53:56 +0200

    feat: created dedicated components for container and header

---
+++ src/components/PageContainer.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="container mx-auto"><slot /></div>
+</template>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,8 @@
+<template>
+  <header><slot /></header>
+</template>
+
+<style lang="sass" scoped>
+header
+  @apply bg-zinc-100
+</style>
